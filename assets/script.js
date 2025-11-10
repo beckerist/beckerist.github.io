8da0e050e24fd3097bc5aebbab14ceca9290d49f
beckerist <github@beckerist.com>
2025-11-10 13:04:17 -0500

    a

--- assets/script.js
+++ assets/script.js
@@ -14,6 +14,7 @@ let score = 0;
 const slingshotBaseX = canvas.width / 2;
 const slingshotBaseY = canvas.height - 150; // Slingshot at the bottom
 const ballRadius = 30;
+const debugBox = -pullDistance;
 let ballY = slingshotBaseY; // Start directly at the string
 let ballX = slingshotBaseX;
 let ballVelocityY = 0;
@@ -130,6 +131,7 @@ function resetGame() {
     ballVelocityX = 0;
     ballVelocityY = 0;
     score = 0;
+    isDragging = false;
     drawUI();
     drawSlingshot();
     drawHook();
